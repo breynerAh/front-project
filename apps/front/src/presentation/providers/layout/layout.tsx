2d4bc3fb39a0d5e40a666045a59844f9ae0423e4
BreynerAcosta <breinerjk788@gmail.com>
2024-10-26 12:52:40 -0500

    cambios nuevos

--- apps/front/src/presentation/providers/layout/layout.tsx
+++ apps/front/src/presentation/providers/layout/layout.tsx
@@ -1,47 +1,21 @@
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import { DrawerComponent } from "./drawer";
-import { Aside } from "./aside";
-import { Header } from "./header";
-import { Outlet } from "react-router-dom";
-import { useMediaQuery } from "@mui/material";
 import clsx from "clsx";
+import { motion } from "framer-motion";
+import { Outlet } from "react-router-dom";
+import { Header } from "./header";
 
 export function Layout() {
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-  const isSmallScreen = useMediaQuery("(max-width: 768px)");
-
-  const handleAsideToggle = () => {
-    !isSmallScreen
-      ? setIsMenuOpen(!isMenuOpen)
-      : setIsDrawerOpen(!isDrawerOpen);
-  };
-
-  useEffect(() => {
-    if (isSmallScreen) {
-      setIsMenuOpen(true);
-    } else {
-      setIsMenuOpen(true);
-      setIsDrawerOpen(false);
-    }
-  }, [isSmallScreen]);
-
   return (
     <div className="w-[100dvw] h-[100dvh] flex">
-      <DrawerComponent open={isDrawerOpen} openChange={handleAsideToggle} />
-      <Aside isMenuOpen={isMenuOpen} />
+      {/* <DrawerComponent open={isDrawerOpen} openChange={handleAsideToggle} /> */}
+      {/* <Aside isMenuOpen={isMenuOpen} /> */}
       <motion.div
         className="w-full flex flex-col"
-        animate={{ marginLeft: isMenuOpen ? 0 : "-350px" }}
-        transition={{ duration: 0.3 }}
+        // animate={{ marginLeft: isMenuOpen ? 0 : "-350px" }}
+        // transition={{ duration: 0.3 }}
       >
-        <Header handleAsideToggle={handleAsideToggle} />
+        <Header />
         <div className="flex flex-1 p-2 bg-muted/40 justify-center overflow-y-scroll">
-          <div
-            className={clsx("w-[98%] h-full")}
-          >
+          <div className={clsx("w-[98%] h-full")}>
             <Outlet />
           </div>
         </div>
--- apps/front/src/presentation/providers/layout/menu.tsx
+++ apps/front/src/presentation/providers/layout/menu.tsx
@@ -1,25 +1,55 @@
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import { BoxUI } from "@repo/ui";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { BoxUI, ButtonUI, TypographyUI } from "@repo/ui";
+// import { useNavigate } from "react-router-dom";
 
-export function Menu() {
-  const navigate = useNavigate();
+export function MenuLayout() {
+  // const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <BoxUI sx={{ width: 250 }} role="presentation">
-      <List>
-        {["Todo", "Pokemon"].map((text, index) => (
-          <ListItem
-            key={text + "_" + index}
-            disablePadding
-            onClick={() => navigate(`/app/${text}`)}
+    <BoxUI sx={{ width: 250, height: "100%" }} role="presentation">
+      <BoxUI
+        sx={{
+          display: "flex",
+          height: "100%",
+          alignItems: "center",
+        }}
+      >
+        <TypographyUI sx={{ color: "white" }}>Principal</TypographyUI>
+        <BoxUI>
+          <ButtonUI
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
           >
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+            Biblioteca
+          </ButtonUI>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </BoxUI>
+      </BoxUI>
     </BoxUI>
   );
 }
