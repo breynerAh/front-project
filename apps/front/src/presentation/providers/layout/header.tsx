5c9ee7332edbd86193fdb66044136702cc607304
JuniorDev <bbarraza@wowdesarrollos.com>
2024-12-10 09:30:38 -0500

    nevos cambios

--- apps/front/src/presentation/providers/layout/header.tsx
+++ apps/front/src/presentation/providers/layout/header.tsx
@@ -1,15 +1,12 @@
 import { BoxUI } from "@repo/ui";
 import { ThemeColor } from "../theme/theme";
 import { MenuLayout } from "./menu";
-import MegaMenu from "@/presentation/providers/layout/ejemplo";
-
 export function Header() {
   const theme = ThemeColor();
 
   return (
     <BoxUI sx={{ height: "80px", background: theme.primary.dark }}>
-      {/* <MenuLayout /> */}
-      <MegaMenu />
+      <MenuLayout />
     </BoxUI>
   );
 }
